c54e2c7edb89b4aa57e9c13d7da3689e2cc2432d
Robert <robertjoonkim@gmail.com>
2012-09-10 17:51:53 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,16 @@ describe "User pages" do
 
   subject { page }
 
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: 'Sign up') }
+  end
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
@@ -34,6 +42,16 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit}
+
+        let(:user) { User.find_by_email("user@example.com") }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
